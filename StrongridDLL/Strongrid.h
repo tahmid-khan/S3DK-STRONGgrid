978c66f829a9216d4c407efd8dc1fb7e19f9b7a2
Tahmid A. Khan <60913202+tahmid-khan@users.noreply.github.com>
2024-09-24 14:12:41 +0600

    Remove typedefs of `int{size}_t`s
    to avoid possibly redefining them using different underlying types than
    those used int `<cstdint>`, and just use `<cstdint>` instead

--- StrongridDLL/Strongrid.h
+++ StrongridDLL/Strongrid.h
@@ -24,15 +24,8 @@
 #define STRONGRIDIEEEC37118DLL_H
 
 
+#include <cstdint>
 typedef unsigned char      BOOL8_t;
-typedef signed char        int8_t;
-typedef short              int16_t;
-typedef int                int32_t;
-typedef long long          int64_t;
-typedef unsigned char      uint8_t;
-typedef unsigned short     uint16_t;
-typedef unsigned int       uint32_t;
-typedef unsigned long long uint64_t;
 
 #define STRONGRIDIEEEC37118DLL_API extern "C"
 
